ef31f6252a481392cfa8731b4e16278120546cf6
roshanmandal45 <kumarroshan1807@gmail.com>
2026-02-04 11:18:09 +0545

    Access

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,19 +1,24 @@
-// firebase.js
+// Import Firebase modules
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+// Firebase config from env variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "moviesuggestion-d6b45.firebaseapp.com",
-  projectId: "moviesuggestion-d6b45",
-  storageBucket: "moviesuggestion-d6b45.firebasestorage.app",
-  messagingSenderId: "182351124866",
-  appId: "1:182351124866:web:0b0b9f419d0ff9358b979b",
-  measurementId: "G-8382J0PJ16",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
+
+// Auth and provider
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider, signInWithPopup };
+// Export everything
+export { app, auth, provider, signInWithPopup };
